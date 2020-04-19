1b472e0dfdd745f6e29feb724a020357bbee54e9
Frisco Nirwana Putra <friscoputra.id@gmail.com>
2020-04-19 08:05:43 +0700

    Fix get full url featured image program

--- app/Program.php
+++ app/Program.php
@@ -3,9 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Program extends Model
 {
+    public function getFeaturedImageAttribute($image)
+    {
+        return Storage::url($image);
+    }
+
     public function updates()
     {
         return $this->hasMany('App\ProgramUpdate');
