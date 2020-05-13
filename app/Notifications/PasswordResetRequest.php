baebc32da8d704f5e68dc1cbee77659f73a2b23a
Frisco Nirwana Putra <37447797+frisconp@users.noreply.github.com>
2020-05-14 00:13:01 +0700

    Create, find, and reset user password

---
+++ app/Notifications/PasswordResetRequest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class PasswordResetRequest extends Notification
+{
+    use Queueable;
+
+    protected $token;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $url = url('api/password/find/'.$this->token);
+
+        return (new MailMessage)
+                    ->line('Kami baru saja menerima permintaan Reset Password untuk akun Anda, klik tombol di bawah ini untuk melakukan Reset Password.')
+                    ->action('Reset Password', url($url))
+                    ->line('Jika Anda merasa tidak melakukannya, harap abaikan e-mail ini.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
