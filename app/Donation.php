6563153ea2d69f709f55060716b77cda16034c04
Frisco Nirwana Putra <37447797+frisconp@users.noreply.github.com>
2020-05-21 18:48:39 +0700

    Fix api list donation

--- app/Donation.php
+++ app/Donation.php
@@ -12,7 +12,11 @@ class Donation extends Model
     ];
 
     protected $appends = [
-        'payment_deadline',
+        'payment_deadline', 'donation_date',
+    ];
+
+    protected $hidden = [
+        'created_at', 'updated_at'
     ];
 
     public function user()
@@ -65,5 +69,27 @@ class Donation extends Model
         $splitedTime = explode(':', date('H:i', strtotime($paymentDeadline)));
 
         return $days[$dayName] . ', ' . $splitedDate[2] . ' ' . $months[(int) $splitedDate[1]] . ' ' . $splitedDate[0] . ' ' . $splitedTime[0] . ':' . $splitedTime[1] . ' WIB';
+    }
+
+    public function getDonationDateAttribute()
+    {
+        $months = [
+            1 => 'Januari',
+            'Februari',
+            'Maret',
+            'April',
+            'Mei',
+            'Juni',
+            'Juli',
+            'Agustus',
+            'September',
+            'Oktober',
+            'November',
+            'Desember'
+        ];
+
+        $splitedDate = explode('-', date('Y-m-d', strtotime($this->created_at)));
+
+        return $splitedDate[2] . ' ' . $months[(int) $splitedDate[1]] . ' ' . $splitedDate[0];
     }
 }
